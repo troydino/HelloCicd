92f9668ceb5420406125b01f678e516cc1470c79
troydino <46838563+troydino@users.noreply.github.com>
2026-04-27 09:36:35 +0100

    Feature/app environment (#12)
    * Add Dockerfile, .dockerignore and CI pipeline
    
    * Add AppEnvironment variable through CI/CD pipeline
    
    * Add AppEnvironment variable through CI/CD pipeline2
    
    ---------
    
    Co-authored-by: WarrenChiptech <warren.smith@chiptech.com>

--- Dockerfile
+++ Dockerfile
@@ -21,6 +21,9 @@ RUN dotnet publish HelloCicd.Api/HelloCicd.Api.csproj \
 FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS runtime
 WORKDIR /app
 
+ARG APP_ENVIRONMENT=Docker
+ENV AppEnvironment=$APP_ENVIRONMENT
+
 USER app
 
 COPY --from=build --chown=app /app/publish .
--- HelloCicd.Tests/Services/HelloServiceTests.cs
+++ HelloCicd.Tests/Services/HelloServiceTests.cs
@@ -1,16 +1,27 @@
 ﻿using HelloCicd.Api.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace HelloCicd.Tests.Services;
 
 public sealed class HelloServiceTests
 {
-    private readonly HelloService _sut = new();
+    private readonly HelloService _sut;
+
+    public HelloServiceTests()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                { "AppEnvironment", "Test" }
+            })
+            .Build();
+
+        _sut = new HelloService(configuration);
+    }
 
     [Fact]
     public void GetHello_WhenCalled_ReturnsHelloWorldMessage()
     {
-        // Arrange - done in constructor
-
         // Act
         var result = _sut.GetHello();
 
@@ -22,14 +33,22 @@ public sealed class HelloServiceTests
     [Fact]
     public void GetHello_WhenCalled_ReturnsVersion()
     {
-        // Arrange - done in constructor
-
         // Act
         var result = _sut.GetHello();
 
         // Assert
         Assert.NotNull(result.Version);
         Assert.NotEmpty(result.Version);
+    }
+
+    [Fact]
+    public void GetHello_WhenCalled_ReturnsAppEnvironment()
+    {
+        // Act
+        var result = _sut.GetHello();
+
+        // Assert
+        Assert.Equal("Test", result.AppEnvironment);
     }
 
     [Fact]
